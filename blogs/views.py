04249fbd1e533844e9b6bf4a16e98bcb17c2f4e6
disizmayur <guptamayur486@gmail.com>
2020-05-19 18:17:36 +0530

    added Bootstrap file

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .models import blog
 # Create your views here.
+def allblogs(request):
+    blo=blog.objects
+    return render(request,'blogs/blogs.html',{'blo':blo})
+
+def details(request, blog_id):
+    dblog=get_object_or_404(blog ,pk=blog_id)
+    return render(request,'details.html',{'blog':dblog})
